c135c2d78e977845149772dfb5d409bed8767b89
AleksanderPotocnik <aleksander.potocnik1@student.um.si>
2026-03-18 12:17:29 +0100

    Dodana funkcija za branje datoteke

--- Vaja2/main.cpp
+++ Vaja2/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
 
-int main()
+bool Branje_Stevil(vector<unsigned char>& vec, const char s[]) {
+    ifstream input(s);
+    int st;
+
+    if (!input.is_open()) {
+        return false;
+    }
+
+    while (!input.eof()) {
+        input >> st;
+        vec.push_back(st);
+        while (isspace(input.peek())) input.get();
+    }
+    input.close();
+    return true;
+}
+
+
+int main(int argc, const char* argv[]) {
 {
-	std::cout << "Hello World!\n";
+    vector<unsigned char> A;
+
+    if (argc < 2) return 0;
+    if (!Branje_Stevil(A, argv[1])) return 0;
+
+
+    return 0;
 }
